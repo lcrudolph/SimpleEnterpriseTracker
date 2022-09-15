d1f7fba1458d53c3b0ff1105e88c054909aebc5b
lcrudolph <33425086+lcrudolph@users.noreply.github.com>
2022-09-15 01:17:08 +0000

    this is the change that includes the database creation code done

---
+++ Project/about/views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint,render_template,redirect,url_for
+from Project import db
+from Project.models import Component
+
+about_blueprint = Blueprint('about', __name__,template_folder='templates')
+
+@about_blueprint.route("/info")
+def info():
+    """Testing a method that what is shown"""
+    return render_template('info.html')
+    
+@about_blueprint.route("/")
+def info2():
+    """Testing a method that what is shown"""
+    return render_template('info.html')
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Lance Rudolph. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
+from Project import app
 
 @app.route("/")
 def hello():
+    """Testing a method that is the default page of the site"""
     return app.send_static_file("index.html")
